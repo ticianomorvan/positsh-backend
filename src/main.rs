01f4e348fc5432c635cfe74570aedd3313d7a787
Ticiano Morvan <ticiano.morvan@gmail.com>
2023-04-07 19:53:42 -0300

    update: add CORS

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ mod handlers;
 mod models;
 
 use ::config::Config;
-use actix_web::{web, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{http::header, web, App, HttpServer};
 use dotenv::dotenv;
 use tokio_postgres::NoTls;
 
@@ -23,14 +24,27 @@ async fn main() -> std::io::Result<()> {
     let pool = config.postgres.create_pool(None, NoTls).unwrap();
 
     let server = HttpServer::new(move || {
-        App::new().app_data(web::Data::new(pool.clone())).service(
-            web::scope("/posits")
-                .service(handlers::get_posits)
-                .service(handlers::create_posit)
-                .service(handlers::get_posit)
-                .service(handlers::update_posit)
-                .service(handlers::delete_posit),
-        )
+        App::new()
+            .wrap(
+                Cors::default()
+                    .allowed_origin("http://localhost:5173")
+                    .allowed_methods(vec!["GET", "POST", "PATCH", "DELETE"])
+                    .allowed_headers(vec![
+                        header::ACCEPT,
+                        header::AUTHORIZATION,
+                        header::CONTENT_TYPE,
+                    ])
+                    .max_age(3600),
+            )
+            .app_data(web::Data::new(pool.clone()))
+            .service(
+                web::scope("/posits")
+                    .service(handlers::get_posits)
+                    .service(handlers::create_posit)
+                    .service(handlers::get_posit)
+                    .service(handlers::update_posit)
+                    .service(handlers::delete_posit),
+            )
     })
     .bind(config.server_address.clone())?
     .run();
